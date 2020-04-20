0a6162186355374bd2a314ff95a7cbc944757a29
BaskaKlim <baska.klimekova@gmail.com>
2020-04-20 22:40:24 +0200

    test scenario, tests package

---
+++ ClickMeBaby/src/test/java/tests/ClickMeBabyTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import java.util.concurrent.locks.*;
+import org.junit.*;
+
+public class ClickMeBabyTest {
+
+    @Test
+    void itShouldDisplayOneClick() {
+        //1. open the website
+        //2. click to button "click me baby"
+        //3. validate the number of clicks
+        //4.validate text "klik"
+
+    }
+
+}
