53a7042c7506592521588a7912bbcb7288cabef5
BaskaKlim <baska.klimekova@gmail.com>
2020-04-20 23:08:31 +0200

    simple test for opening website, click on button and check the text

--- ClickMeBaby/src/test/java/tests/ClickMeBabyTest.java
+++ ClickMeBaby/src/test/java/tests/ClickMeBabyTest.java
@@ -1,17 +1,25 @@
+
 package tests;
 
-import java.util.concurrent.locks.*;
-import org.junit.*;
+import org.junit.jupiter.api.*;
+
+import com.codeborne.selenide.*;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class ClickMeBabyTest {
 
     @Test
     void itShouldDisplayOneClick() {
         //1. open the website
+        open("http://ajtyvit-app.westeurope.cloudapp.azure.com:8080/clickmebaby.php");
         //2. click to button "click me baby"
+        $(byId("clickMe")).click();
         //3. validate the number of clicks
+        $(byId("clicks")).shouldHave(Condition.text("1"));
         //4.validate text "klik"
+        $(byCssSelector("p.description")).shouldHave(Condition.text("klik"));
 
     }
-
 }
