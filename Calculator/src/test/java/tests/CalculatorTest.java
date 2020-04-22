1897d519b6148ca5c9e2f82b07f36328ae18464b
BaskaKlim <baska.klimekova@gmail.com>
2020-04-22 17:28:55 +0200

    new test for calculator - structure of tests

---
+++ Calculator/src/test/java/tests/CalculatorTest.java
@@ -0,0 +1,13 @@
+
+package tests;
+
+import org.junit.jupiter.api.*;
+
+import com.codeborne.selenide.*;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CalculatorTest {
+    
+}
