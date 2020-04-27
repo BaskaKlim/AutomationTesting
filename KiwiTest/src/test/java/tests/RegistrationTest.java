039bfeea91ce93c088cdd1a2bf509045e18073f9
BaskaKlim <baska.klimekova@gmail.com>
2020-04-27 21:26:34 +0200

    handling cookies in kiwi.com/en website

--- KiwiTest/src/test/java/tests/RegistrationTest.java
+++ KiwiTest/src/test/java/tests/RegistrationTest.java
@@ -5,13 +5,25 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 
 import com.codeborne.selenide.*;
-
-import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationTest extends TestBase {
 
     @BeforeEach
     void openPage() {
+        Configuration.holdBrowserOpen = true;
+    }
+    @AfterAll
+    void  closePage(){
+        closeWebDriver();
+    }
 
+    @Test
+    void itShouldDisplaySuggestedTrip() {
+        open("https://www.kiwi.com/en/");
+        //cookies
+        Cookie cookie = new Cookie("cookie_consent", "agreed");
+        WebDriverRunner.getWebDriver().manage().addCookie(cookie);
+        refresh();
+    }
 }
