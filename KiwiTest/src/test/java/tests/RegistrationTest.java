60e9efc1e5d6dc2cfce36562ae75a90e20631682
BaskaKlim <baska.klimekova@gmail.com>
2020-04-27 22:41:05 +0200

    itShouldDisplaySuggestedTrip() test passed

--- KiwiTest/src/test/java/tests/RegistrationTest.java
+++ KiwiTest/src/test/java/tests/RegistrationTest.java
@@ -15,22 +15,29 @@ public class RegistrationTest extends TestBase {
     void openPage() {
         Configuration.holdBrowserOpen = true;
     }
-    @AfterAll
-    void  closePage(){
-        closeWebDriver();
-    }
 
     @Test
     void itShouldDisplaySuggestedTrip() {
         open("https://www.kiwi.com/en/");
-        //cookies
+
+        // TODO: cookies
         Cookie cookie = new Cookie("cookie_consent", "agreed");
         WebDriverRunner.getWebDriver().manage().addCookie(cookie);
         refresh();
 
-         //popup windows clicked by finding elements by text
+        //TODO: popup windows clicked by finding elements by text
         $(byText("I understand")).click();
+
+        //TODO: verify expected result
+        //predvolena destinacia je vo formulari zadana automaticky, staci kliknut na button explore
         $(byText("Explore")).click();
+        //overi image not result
+        $(byAttribute("alt", "no results")).shouldBe(Condition.visible);
+        //overi text co sa zobrazuje na stranke ako chybova hlaska
+        $(byAttribute("data-test", "AggregateResultsWrapper"))
+                .shouldHave(Condition.text("We couldn't find your trip."));
 
     }
+
 }
+
